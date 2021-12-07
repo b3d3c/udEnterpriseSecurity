f15010e54152525580b8447601a8a8641fe215e9
b3d3c <46558799+b3d3c@users.noreply.github.com>
2021-12-07 11:13:42 +0100

    Add files via upload

---
+++ entsec.sh
@@ -0,0 +1,7 @@
+sudo apt update
+sudo apt install docker.io
+sudo apt install python3-pip
+sudo pip3 install docker
+sudo sysctl -w vm.max_map_count=262144
+sudo docker pull sebp/elk:761 
+sudo docker run -p 5601:5601 -p 9200:9200 -p 5044:5044 -it --name elk sebp/elk:761
